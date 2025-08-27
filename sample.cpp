3e0b50d738d22c705cd2cb7ddbe30791a8272d30
KaiserFelwinter <Kalonjigv2@gmail.com>
2025-08-27 13:22:45 -0400

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,16 @@
-// YOUR NAME HERE
+// Kalonji George
 #include <iostream>
 using namespace std;
 
 int main () {
+  int num1;
+  int num2;
+
+  cout << "Please enter your first integer: ";
+  cin >> num1;
+  cout << "Please enter your second integer: ";
+  cin >> num2;
+
+  cout <<"The integer sum is "<< num1+ num2;
 
 }
